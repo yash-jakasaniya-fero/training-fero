1f2ae19855836d9246c693e09dc7635e31246046
yash <yash.jakasaniya@fero.ai>
2025-01-21 16:26:29 +0530

    Oops Task Done

---
+++ blog_app/app1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
